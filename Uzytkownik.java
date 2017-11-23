9d15217f3fe69972f323c22f4bbce7923865dad7
pokyfudywise <nerlo.karol@gmail.com>
2017-11-24 00:45:53 +0100

    Revert "second iteration"
    This reverts commit 220880967dd88574ec5ca04835f07c31ba803293.

--- Uzytkownik.java
+++ Uzytkownik.java
@@ -1,32 +1,24 @@
 package app;
 
 public class Uzytkownik {
-    private String imie;
-    private String nazwisko;
-    private String pesel;
-    private boolean jestKierowca = false;
-    private int punktyKarne = 0;
+    String imie;
+    String nazwisko;
+    String pesel;
+    boolean jestKierowca = false;
+    int punktyKarne = 0;
     
 
-    public Uzytkownik(String i, String n, String p){
-        this.imie = i;
-        this.nazwisko = n;
-        this.pesel = p;
+    public Uzytkownik(String i, String n, String p){ //konstruktor
+        imie = i;
+        nazwisko = n;
+        pesel = p;
     }
     
     public boolean getJestKierowca(){ 
-        return this.jestKierowca;
+        return jestKierowca;
     }
     public void setJestKierowca(boolean jk){ 
         this.jestKierowca = jk;
-    }
-
-    public void zmienIloscPunktow(int pktKarne){
-        this.punktyKarne += pktKarne;
-    }
-
-    public void kasujPunkty(int pktKarne){
-        this.punktyKarne = pktKarne;
     }
     
     public Boolean sprawdzPesel(String pesel){
@@ -36,11 +28,8 @@ public class Uzytkownik {
         }
         return false;
     }
-    public int getPunktyKarne(){
-        return this.punktyKarne;
-    }
     
-    public String toString(){
+    public String toString(){ //funkcja przedefiniowana przez programistę
         String wynik = "Uzytkownik: \nImie: " + this.imie + "\nNazwisko: " + this.nazwisko + "\nPesel: " 
                 + this.pesel + "\nCzy jest kierowca: " + this.jestKierowca + "\nPunkty: "
                 + this.punktyKarne + "\n";
